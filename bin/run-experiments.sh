b8f502e7ddfaa971437d14bb8d45b184670b41f0
Fenella McAndrew <fenellamcandrew@gmail.com>
2020-06-16 10:37:02 +1000

    updated shell run file

--- bin/run-experiments.sh
+++ bin/run-experiments.sh
@@ -2,4 +2,11 @@
 
 set -e
 run_file="params/ready/t_step0.010000__time_T10__instance_index1.000000__n_qubits4.000000__graph_typeSparse.yml"
-sbatch --mem 10GB --output=tests.log bin/run-experiments.slurm $run_file
+
+dir=params/ready
+for f in $dir/*
+do
+  local_run_path=$f
+  echo $local_run_path
+done
+#sbatch --mem 10GB --output=tests.log bin/run-experiments.slurm $run_file
